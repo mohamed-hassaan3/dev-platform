3440ab8a7a6a35e67b97470d3ff4753f23ac4af0
Mohamed Hassaan <mmhassaan3@gmail.com>
2025-03-20 01:11:19 +0400

    add UI for signup form working on Aceternity UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressContentEditableWarning
       >
-        {children}
+
+            {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+import SignupForm from "@/components/signup-form";
 import Button from "@/components/ui/buttons/Button";
 import Link from "next/link";
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarLink,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 
 export default function Home() {
   return (
-    <div className="font-semibold text-center grid justify-center items-center min-h-screen">
-      <h1 className="text-4xl">Hello Prisma Docker and Postgresql </h1>
+    <div className="">
+      <SignupForm />
+      {/* <SidebarProvider>
+        <SidebarBody>
+          <Sidebar>
+            <SidebarLink
+              link={{ href: "/profile", label: "Home", icon: <span>🏠</span> }}
+            ></SidebarLink>
+          </Sidebar>
+        </SidebarBody>
+      </SidebarProvider> */}
+
+      
+      {/* <h1 className="text-4xl">Hello Prisma Docker and Postgresql </h1>
       <div className="gap-16 flex justify-center items-center">
         <Link className="w-full" href={`/profile`}>
           <Button size="large" color="primary" className="text-black w-full">
@@ -16,7 +35,7 @@ export default function Home() {
             Feed
           </Button>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 }
